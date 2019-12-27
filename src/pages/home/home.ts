6a225f5fba766ac4b58f39450070390f330a3033
kurotabo <kurotabo@users.noreply.github.com>
2019-12-27 08:44:19 -0300

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+import mapboxgl from 'mapbox-gl';
+import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,41 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  directions: any;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad() {
+
+    this.InitializeMapBox();
+  }
+
+  InitializeMapBox(){
+
+    var happy = new mapboxgl.LngLat(-47.9292, -15.7801);
+ 
+    mapboxgl.accessToken = 'pk.eyJ1IjoieXVraXRha2V1dGkiLCJhIjoiY2s0ZTZyZTltMGFncjNubzZxMmVmbzd0NCJ9.Cfe08VfXHYG51f3PweX2WQ';
+    var map = new mapboxgl.Map({
+    container: 'map',
+    center: happy,
+    style: 'mapbox://styles/mapbox/streets-v11',
+    zoom: 9,
+    });
+
+    var campinas = new mapboxgl.LngLat(-47.0616,-22.9064);
+
+    new mapboxgl.Marker().setLngLat(campinas).addTo(map);
+
+    map.addControl(new MapboxDirections({
+      accessToken: mapboxgl.accessToken, controls: {
+        instructions: false,
+        profileSwitcher: false
+      }
+    }),'top-left');
   }
 
 }
